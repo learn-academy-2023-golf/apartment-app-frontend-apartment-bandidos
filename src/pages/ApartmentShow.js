2f123125f1d1f5b39aed720b331b0240cc1b421c
Greg <gvasquezencalada@gmail.com>
2023-12-14 15:23:11 -0800

    fixed indentation on ApartmentShow.js

--- src/pages/ApartmentShow.js
+++ src/pages/ApartmentShow.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { NavLink, useParams } from 'react-router-dom'
+import React from "react";
+import { NavLink, useParams } from "react-router-dom";
 import {
   CardBody,
   CardTitle,
@@ -9,43 +9,42 @@ import {
   Card,
 } from "reactstrap";
 
-const ApartmentShow = ({apartments}) => {
-
-  let {id} = useParams()
-  const apartment = apartments?.find((apartment) => apartment.id === +id)
+const ApartmentShow = ({ apartments }) => {
+  let { id } = useParams();
+  const apartment = apartments?.find((apartment) => apartment.id === +id);
 
   return (
     <>
-    <Card
-              style={{
-                width: "18rem",
-              }}
-            >
-              <img alt={`image of ${apartment.id}`} src={apartment.image} />
-              <CardBody>
-                <CardTitle tag="h5">
-                  Unit: {apartment.unit}, {apartment.street}, {apartment.city},{" "}
-                  {apartment.state}.
-                </CardTitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                  ${apartment.price}
-                </CardSubtitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                  Bedrooms: {apartment.bedrooms} / Bathrooms: {apartment.bathrooms}
-                </CardSubtitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                  Square footage: {apartment.square_footage}
-                </CardSubtitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                  Pets: {apartment.pets}
-                </CardSubtitle>
-                <NavLink to={`/apartmentindex`}>
-                  <Button>Back to listing</Button>
-                </NavLink>
-              </CardBody>
-            </Card>
-            </>
-  )
-}
+      <Card
+        style={{
+          width: "18rem",
+        }}
+      >
+        <img alt={`image of ${apartment.id}`} src={apartment.image} />
+        <CardBody>
+          <CardTitle tag="h5">
+            Unit: {apartment.unit}, {apartment.street}, {apartment.city},{" "}
+            {apartment.state}.
+          </CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            ${apartment.price}
+          </CardSubtitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            Bedrooms: {apartment.bedrooms} / Bathrooms: {apartment.bathrooms}
+          </CardSubtitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            Square footage: {apartment.square_footage}
+          </CardSubtitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            Pets: {apartment.pets}
+          </CardSubtitle>
+          <NavLink to={`/apartmentindex`}>
+            <Button>Back to listing</Button>
+          </NavLink>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
 
-export default ApartmentShow
+export default ApartmentShow;
