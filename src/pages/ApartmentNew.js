051e4abe38906091528f3f512b20ffcc90fae5cd
Greg <gvasquezencalada@gmail.com>
2023-12-15 16:05:24 -0800

    Added functionality, testing, and styling

--- src/pages/ApartmentNew.js
+++ src/pages/ApartmentNew.js
@@ -1,9 +1,111 @@
-import React from 'react'
+import React from "react";
+import { Form, FormGroup, Label, Input, Col, Button, Row } from "reactstrap";
+import "../App.css";
 
 const ApartmentNew = () => {
   return (
-    <div>ApartmentNew</div>
-  )
-}
+    <div>
+      <Form>
+        <Row>
+          <Col md={4}>
+            <FormGroup>
+              <Label for="exampleAddress">Address</Label>
+              <Input
+                id="exampleAddress"
+                name="address"
+                placeholder="1234 Main St"
+              />
+            </FormGroup>
 
-export default ApartmentNew
+            <Row>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="exampleCity">City</Label>
+                  <Input id="exampleCity" name="city" />
+                </FormGroup>
+
+                <FormGroup>
+                  <Label for="exampleAddress2">Square footage</Label>
+                  <Input id="exampleAddress2" name="address2" placeholder="" />
+                </FormGroup>
+              </Col>
+              <Col md={4}>
+                <FormGroup>
+                  <Label for="exampleState">State</Label>
+                  <Input id="exampleState" name="state" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="examplePrice">Price</Label>
+                  <Input
+                    id="examplPrice"
+                    name="Price"
+                    placeholder="$0.00"
+                    type="text"
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={3}>
+                <FormGroup>
+                  <Label for="examplePassword">Bed</Label>
+                  <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="0"
+                    type="number"
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={3}>
+                <FormGroup>
+                  <Label for="examplePassword">Bath</Label>
+                  <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="0"
+                    type="number"
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={10}>
+                <FormGroup>
+                  <Label for="examplePassword">Pets</Label>
+                  <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="Yes, hippopotamuses only"
+                    type="text"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+            <FormGroup>
+                  <Label for="examplePassword">Image URL</Label>
+                  <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="URL"
+                    type="text"
+                  />
+                </FormGroup>
+            </Row>
+          </Col>
+          <Col md={2}>
+            <FormGroup>
+              <Label for="exampleZip">Unit</Label>
+              <Input 
+                    id="examplePassword"
+                    name="password"
+                    placeholder="URL"
+                    type="text" />
+            </FormGroup>
+          </Col>
+        </Row>
+        
+        <Button>Sign in</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default ApartmentNew;
